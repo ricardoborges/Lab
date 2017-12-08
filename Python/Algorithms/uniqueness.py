16b8d89f93a33ffe4cd2aa8858681ef28774f0c2
= <ricardoborges@gmail.com>
2017-12-08 15:47:22 -0300

    rename

---
+++ Python/Algorithms/uniqueness.py
@@ -0,0 +1,15 @@
+def unique1(S):
+    """ O(n**2) """
+    for j in range(len(S)):
+        for k in range(j+1, len(S)):
+            if S[j] == S[k]:
+                return False
+    return True
+
+def unique2(S):
+    """ O(nlogn) """
+    temp = sorted(S)
+    for j in range(1, len(temp)):
+        if S[j-1] == S[j]:
+            return False
+    return True
